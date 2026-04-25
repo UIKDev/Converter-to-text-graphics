0d99f6bfcf8b134b1f333ffa361002192d134216
UIKDev <piraxanine@gmail.com>
2026-04-25 20:55:49 +0300

    Приведение main в порядок

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -8,16 +8,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         TextGraphicsConverter converter = new TextGraphicsConverterImpl(); // Создайте тут объект вашего класса конвертера
 
-//      https://i.ibb.co/k22CKVs3/image2.jpg эталон
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
 
 //         Или то же, но с выводом на экран:
 //        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
+
+//        https://i.ibb.co/k22CKVs3/image2.jpg эталон
 //        https://i.ibb.co/6DYM05G/edu0.jpg
 //        https://i.ibb.co/k22CKVs3/image2.jpg
 //        https://i.ibb.co/DDPDT9JW/out.png
-//        String imgTxt = converter.convert(url);
-//        System.out.println(imgTxt);
+
     }
 }
